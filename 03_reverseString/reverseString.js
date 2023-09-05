41dbf854593475e19667ffa980033543ae1e909e
turner.jeremy <37211811+turner-jeremy@users.noreply.github.com>
2023-09-05 15:44:07 +0800

    Complete exercise 01-04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
-};
+function reverseString(string) {
+    return string.split("").reverse().join("");
+}
 
 // Do not edit below this line
 module.exports = reverseString;
+
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
 const removeFromArray = function() {
+    
+    const array = arguments[0]; // Array of 1st argument which will be original array
 
+    // Create new array for any arguments to be removed (index > 0)
+    const removeItem = []; 
+    for (let i = 1; i < arguments.length; i++) {
+        removeItem.push(arguments[i]);
+    }
+
+    // Loop through original array and remove matching items
+    for (let r of removeItem) {
+        const index = array.indexOf(r);
+        if (index > -1) {
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// npm test removeFromArray.spec.js
