31ae88cac9be7e08bead21af5584343dd9c11f72
turner.jeremy <37211811+turner-jeremy@users.noreply.github.com>
2023-09-05 16:32:26 +0800

    Complete exercises 05-07
    All exercises are passing tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
+    if (!Number.isInteger(startInt) || !Number.isInteger(endInt)) return 'ERROR';
+    if (startInt < 0 || endInt < 0) return 'ERROR';
+    
+    if (startInt < endInt) {
+        var min = startInt;
+        var max = endInt;
+    } else {
+        var min = endInt;
+        var max = startInt;
+    }
 
+    var sumOutput = 0
+    for (i = min; i<= max; i++) {
+        sumOutput += i;
+    }
+    return sumOutput;
 };
 
 // Do not edit below this line
